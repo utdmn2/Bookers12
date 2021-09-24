73bcb43ffb0d37b5ef4f40d3283f6f654af80e52
utdmn2 <utdmn2@gmail.com>
2021-09-24 23:58:30 +0000

    [Finish]課題２復習

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,20 @@
 class BooksController < ApplicationController
-   
+
   def index
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = 'Book was successfully created.'
+    redirect_to book_path(@book.id)
+    else
+    render :index
+    end
+
   end
 
   def show
@@ -20,17 +26,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = 'Book was successfully updated.'
+    redirect_to book_path(@book.id)
+    else
+    render :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path ,notice: "Book was successfully destroyed."
   end
-  
+
 private
 
 def book_params
